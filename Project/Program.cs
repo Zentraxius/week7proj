dc01aa490ba675acabb89886482735f81132c347
Cody Fritz <clanalia55@gmail.com>
2020-07-18 13:44:08 -0700

    attempt new method to perform front-end logic in program.cs

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,34 +1,38 @@
-using Project;
 using System;
+using Project;
 using System.Collections.Generic;
 
-
-class Program
+namespace Project
 {
-  static void Main()
+  class Program
   {
-    Console.WriteLine("Hello, welcome to Pierre's Bakery.");
-    Console.WriteLine("Bread is $5 a loaf, and has a buy 2 get 1 free deal!");
-    Console.WriteLine("Pastries, are only $2 each and have a 3 for $5 deal!");
-
-  }
-  private static int BreadPrice(int amount)
-  {
-    Console.WriteLine("How many loaves of bread would you like to order?");
-    string userInput = Console.ReadLine();
-    int priceOfBread;
-    bool canBeParsed = int.TryParse(userInput, out priceOfBread);
-
-    if (canBeParsed)
+    static void Main()
     {
-      priceOfBread = int.Parse(userInput);
-      Bread breadOrder = new Bread(priceOfBread);
-      Console.WriteLine("You have ordered: " + userInput + "loaves of bread for a total of " + breadOrder);
-      return breadOrder;
+      Console.WriteLine("Hello, welcome to Pierre's Bakery.");
+      Console.WriteLine("Bread is $5 a loaf, and has a buy 2 get 1 free deal!");
+      Console.WriteLine("Pastries, are only $2 each and have a 3 for $5 deal!");
+      Console.WriteLine("-----------------------------------------------------");
+      Console.WriteLine("How many loaves of bread would you like?");
+
     }
-    else
-    {
-      Console.WriteLine("Please enter a number");
-    }
+    // private static int BreadPrice(int amount)
+    // {
+    //   Console.WriteLine("How many loaves of bread would you like?");
+    //   string userInputBread = Console.ReadLine();
+    //   int priceOfBread;
+
+    //   bool canBeParsed = int.TryParse(userInputBread, out priceOfBread);
+
+    //   if (canBeParsed)
+    //   {
+    //     int amountToOrderBread = int.Parse(userInputBread);
+    //     Bread breadOrder = new Bread(amountToOrderBread);
+    //     return breadOrder.Price();
+    //   }
+    //   else
+    //   {
+    //     Console.WriteLine("Please enter a valid number");
+    //   }
+    // }
   }
 }
